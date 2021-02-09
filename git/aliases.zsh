e0ab191acbc0e0c921edf69ec3457dca1a61aea6
Alex Okros <alexanderlokros@gmail.com>
2021-02-09 10:58:56 -0500

    feat: add gcod command for checking out develop

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,6 +20,7 @@ alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcob='git checkout -b'
+alias gcod='git checkout develop'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
